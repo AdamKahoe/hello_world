46421d7bf18dbca444aa946fb052393a7dbc8b88
Adam Kahoe <89557570+AdamKahoe@users.noreply.github.com>
2022-03-14 16:21:18 -0700

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+print("Hello, Adam!")
+print("Hello + Adam")
+
+name = "42"
+print("Hello, 42!")
+print("Hello +42!")
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+text = "I love to eat {}!"
+# print("I love {{fave_food1}}!")
+# print("I love {{fave_food2}}!")
+
+print(text.format(fave_food1))
+print(text.format(fave_food2))
+
